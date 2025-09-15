0815e5e42f0f300ffdbecc8718ccdb0297bbb8b9
yunseonhyun <70725625+yunseonhyun@users.noreply.github.com>
2025-09-15 18:04:54 +0900

    Update Day01_SELECT_Practice.sql

--- Day01_SELECT_Practice.sql
+++ Day01_SELECT_Practice.sql
@@ -25,76 +25,112 @@ FROM STUDENT;
 -- 문제 5
 -- 현재 시간과 어제, 내일을 조회하시오.
 -- (컬럼명은 각각 '현재시간', '어제', '내일'로 별칭 지정)
+SELECT NOW() AS "현재시간", NOW() - interval 1 DAY AS "어제", now() + interval 1 DAY AS "내일";
 
 
 -- 문제 6
 -- STUDENT 테이블에서 학번과 이름을 연결하여 하나의 컬럼으로 조회하시오.
 -- (컬럼명은 '학번_이름'으로 별칭 지정)
-
+SELECT CONCAT(STUDENT_NO, STUDENT_NAME) AS "학번_이름"
+FROM STUDENT;
 
 -- 문제 7
 -- STUDENT 테이블에서 존재하는 학과번호의 종류만 중복 없이 조회하시오.
-
+SELECT DISTINCT DEPARTMENT_NO
+FROM STUDENT;
 
 -- 문제 8
 -- GRADE 테이블에서 중복 없이 존재하는 학기번호(TERM_NO) 종류를 조회하시오.
-
+SELECT DISTINCT TERM_NO
+FROM GRADE;
 
 -- 문제 9
 -- STUDENT 테이블에서 휴학여부(ABSENCE_YN)가 'Y'인 학생의 
 -- 학번, 이름, 학과번호를 조회하시오.
-
+SELECT STUDENT_NO, STUDENT_NAME, DEPARTMENT_NO
+FROM STUDENT
+WHERE ABSENCE_YN = 'Y';
 
 -- 문제 10
 -- DEPARTMENT 테이블에서 정원(CAPACITY)이 25명 이상인 학과의 
 -- 학과명, 분류, 정원을 조회하시오.
-
+SELECT DEPARTMENT_NAME, CATEGORY, CAPACITY
+FROM DEPARTMENT
+WHERE CAPACITY >= 25;
 
 -- 문제 11
 -- STUDENT 테이블에서 학과번호가 '001'이 아닌 학생의 
 -- 이름, 학과번호, 주소를 조회하시오.
+SELECT STUDENT_NAME, DEPARTMENT_NO, STUDENT_ADDRESS
+FROM STUDENT
+WHERE DEPARTMENT_NO != 001;
 
 
 -- 문제 12
 -- GRADE 테이블에서 성적(POINT)이 4.0 이상인 성적 데이터의 
 -- 학기번호, 과목번호, 학번, 성적을 조회하시오.
+SELECT TERM_NO, CLASS_NO, STUDENT_NO, POINT
+FROM GRADE
+WHERE POINT >= 4.0;
 
 
 -- 문제 13
 -- STUDENT 테이블에서 2005년에 입학한 학생의 
 -- 학번, 이름, 입학일을 조회하시오.
+SELECT STUDENT_NO, STUDENT_NAME, ENTRANCE_DATE
+FROM STUDENT
+WHERE ENTRANCE_DATE LIKE '2005%';
 
 
 -- 문제 14
 -- PROFESSOR 테이블에서 소속 학과번호(DEPARTMENT_NO)가 NULL이 아닌 교수의 
 -- 교수번호, 이름, 학과번호를 조회하시오.
+SELECT PROFESSOR_NO, PROFESSOR_NAME, DEPARTMENT_NO
+FROM PROFESSOR
+WHERE DEPARTMENT_NO != 'NULL';
 
 
 -- 문제 15
 -- CLASS 테이블에서 과목유형(CLASS_TYPE)이 '전공필수'인 과목의 
 -- 과목번호, 과목명, 과목유형을 조회하시오.
+SELECT DEPARTMENT_NO, CLASS_NAME, CLASS_TYPE
+FROM CLASS
+WHERE CLASS_TYPE = "전공필수";
 
 
 -- 문제 16
 -- STUDENT 테이블에서 주소가 '서울시'로 시작하는 학생의 
 -- 이름, 주소, 입학일을 조회하시오.
+SELECT STUDENT_NAME, STUDENT_ADDRESS, ENTRANCE_DATE
+FROM STUDENT
+WHERE STUDENT_ADDRESS LIKE '서울시%';
 
 
 -- 문제 17
 -- GRADE 테이블에서 성적이 3.0 이상 4.0 미만인 성적 데이터의 
 -- 학번, 과목번호, 성적을 조회하시오.
+SELECT STUDENT_NO, CLASS_NO, POINT
+FROM GRADE
+WHERE 3.0 <= POINT AND POINT <= 4.0;
 
 
 -- 문제 18
 -- STUDENT 테이블에서 지도교수번호(COACH_PROFESSOR_NO)가 'P001'인 학생의 
 -- 학번, 이름, 학과번호를 조회하시오.
-
+SELECT STUDENT_NO, STUDENT_NAME, DEPARTMENT_NO
+FROM STUDENT
+WHERE COACH_PROFESSOR_NO = 'P001';
 
 -- 문제 19
 -- DEPARTMENT 테이블에서 분류(CATEGORY)가 '인문사회'인 학과의 
 -- 학과명, 분류, 개설여부를 조회하시오.
-
+SELECT DEPARTMENT_NAME, CATEGORY, OPEN_YN
+FROM DEPARTMENT
+WHERE CATEGORY = '인문사회';
 
 -- 문제 20
 -- STUDENT 테이블에서 학번에 'A'가 포함된 학생의 
 -- 학번, 이름, 입학일을 조회하시오.
+SELECT STUDENT_NO, STUDENT_NAME, ENTRANCE_DATE
+FROM STUDENT
+WHERE STUDENT_NO LIKE 'A%';
